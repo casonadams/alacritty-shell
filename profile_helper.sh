dae55a35aab55acfc07a344fefd3d7e85a24e5c2
Cason Adams <casonadams@gmail.com>
2021-12-09 01:24:42 -0700

    new theme generator

--- profile_helper.sh
+++ profile_helper.sh
@@ -9,20 +9,20 @@ script_dir=$(cd "$(dirname "$file_name")" && pwd)
 
 . "$script_dir/realpath/realpath.sh"
 
-if [ -f ~/.base16_theme ]; then
-  script_name=$(basename "$(realpath ~/.base16_theme)" .sh)
+if [ -f ~/.walh_theme ]; then
+  script_name=$(basename "$(realpath ~/.walh_theme)" .sh)
   echo "export BASE16_THEME=${script_name#*-}"
-  echo ". ~/.base16_theme"
+  echo ". ~/.walh_theme"
 fi
 cat <<'FUNC'
-_base16()
+_walh()
 {
   local script=$1
   local theme=$2
   [ -f $script ] && . $script
-  ln -fs $script ~/.base16_theme
+  ln -fs $script ~/.walh_theme
   export BASE16_THEME=${theme}
-  echo -e "if \0041exists('g:colors_name') || g:colors_name != 'base16-$theme'\n  colorscheme base16-$theme\nendif" >| ~/.vimrc_background
+  echo -e "if \0041exists('g:colors_name') || g:colors_name != 'walh-$theme'\n  colorscheme walh-$theme\nendif" >| ~/.vimrc_background
   if [ -n ${BASE16_SHELL_HOOKS:+s} ] && [ -d "${BASE16_SHELL_HOOKS}" ]; then
     for hook in $BASE16_SHELL_HOOKS/*; do
       [ -f "$hook" ] && [ -x "$hook" ] && "$hook"
@@ -30,11 +30,11 @@ _base16()
   fi
 }
 FUNC
-for script in "$script_dir"/scripts/base16*.sh; do
+for script in "$script_dir"/scripts/walh*.sh; do
   script_name=${script##*/}
   script_name=${script_name%.sh}
   theme=${script_name#*-}
-  func_name="base16_${theme}"
-  echo "alias $func_name=\"_base16 \\\"$script\\\" $theme\""
+  func_name="walh_${theme}"
+  echo "alias $func_name=\"_walh \\\"$script\\\" $theme\""
 done;
 
