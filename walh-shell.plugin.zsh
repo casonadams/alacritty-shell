37f25d1122ac189ec0c1b53bb594ac3c3d329c9e
Cason Adams <casonadams@gmail.com>
2021-12-13 21:29:13 -0700

    formatting

--- walh-shell.plugin.zsh
+++ walh-shell.plugin.zsh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
-#
-WALH_SHELL=$(dirname ${(%):-%x})
-[ -n "$PS1" ] && [ -s $WALH_SHELL/profile_helper.sh ] && eval "$($WALH_SHELL/profile_helper.sh)"
+
+WALH_SHELL=$(dirname "${(%):-%x}")
+
+[ -n "$PS1" ] \
+    && [ -s "${WALH_SHELL}/profile_helper.sh" ] \
+    && eval "$("${WALH_SHELL}/profile_helper.sh")"
