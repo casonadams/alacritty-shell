6248050ab6a280996d6a30e6c461746faeb5cdbc
Cason Adams <casonadams@gmail.com>
2021-12-27 01:08:58 -0700

    update helper

--- walh-shell.plugin.zsh
+++ walh-shell.plugin.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 ALACRITTY_SHELL=$(dirname "${(%):-%x}")
+
 YQ_VERSION="v4.16.2"
 YQ_URL="https://github.com/mikefarah/yq/releases/download"
 
@@ -8,8 +9,8 @@ if [ ! -f "${ALACRITTY_SHELL}/yq" ]; then
   local uname="$(uname)"
   local arch="$(uname -m)"
 
-  if [ $arch == "x86_64" ]; then
-    curl -L "${YQ_URL}/${YQ_VERSION}/yq_${uname}_amd64" -o "$ALACRITTY_SHELL/yq"
+  if [[ $arch == "x86_64" ]]; then
+    curl -sSL "${YQ_URL}/${YQ_VERSION}/yq_${uname}_amd64" -o "$ALACRITTY_SHELL/yq"
     chmod +x "${ALACRITTY_SHELL/yq}"
   else
     echo "$arch not supported"
@@ -20,3 +21,4 @@ fi
 [ -n "$PS1" ] \
     && [ -s "${ALACRITTY_SHELL}/profile_helper.sh" ] \
     && eval "$("${ALACRITTY_SHELL}/profile_helper.sh")"
+
