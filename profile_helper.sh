a31c0b94b1e241bc16dc1175b132e9504062ef4e
Cason Adams <casonadams@gmail.com>
2022-10-06 01:01:52 -0600

    add: helper function to add builtin themes

--- profile_helper.sh
+++ profile_helper.sh
@@ -12,9 +12,17 @@ fi
 echo "export _YQ_PATH=${_YQ_PATH}"
 
 cat << 'FUNC'
+_add() {
+  local theme="$1"
+
+  "${_YQ_PATH}" -i eval-all '. as $item ireduce ({}; . * $item )' "${_ALACRITTY_YML}" "${_ALACRITTY_SHELL}/themes/${theme}.yml"
+  _alacritty "${theme}"
+}
+
+FUNC
+cat << 'FUNC'
 _alacritty() {
-  local theme="$1" yq
-  yq="${_ALACRITTY_SHELL}/yq"
+  local theme="$1"
 
   "${_YQ_PATH}" -i eval-all ".colors alias = \"${theme}\"" "${_ALACRITTY_YML}"
 }
@@ -23,4 +31,10 @@ FUNC
 for theme in $("${_YQ_PATH}" e '.schemes.[] | anchor' "${_ALACRITTY_YML}"); do
   alacritty_func_name="alacritty_${theme}"
   echo "alias $alacritty_func_name=\"_alacritty $theme\""
+done
+
+for theme in "${_ALACRITTY_SHELL}"/themes/*.yml; do
+  filename=$(basename -s .yml "${theme}")
+  alacritty_func_name="add_alacritty_${filename}"
+  echo "alias $alacritty_func_name=\"_add ${filename}\""
 done
